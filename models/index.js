1ca65b5b9ec533efd4a6df02a817f982141bd4de
tgtiburon <tg.tiburon@gmail.com>
2021-12-20 17:06:54 -0500

    created Post Model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,12 @@
 const User = require('./User');
+const Post = require('./Post');
 
 
 
 
 
-module.exports = { User};
+module.exports = { User, Post};
+
 
 
 // This file imports the User model and exports a User object.
