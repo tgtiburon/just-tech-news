a7233bd53c744fff395a75f3d1c7d1ddbef1a99b
tgtiburon <tg.tiburon@gmail.com>
2022-01-04 14:43:07 -0500

    homepage done

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // This second router.use catches all passthroughs and 404s them.
 router.use((req, res)=> {
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+const path = require('path');
+
+// setup handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// setup app for handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// built-in middle wear.  everything in public is static and can
+// be used without specific api calls
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
