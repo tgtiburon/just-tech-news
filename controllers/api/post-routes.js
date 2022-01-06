d06ca24968bfa486def4ba7eafd8d5ede0d2a20f
tgtiburon <tg.tiburon@gmail.com>
2022-01-05 20:07:50 -0500

    finished edit delete etc

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -86,12 +88,12 @@ router.get('/:id', (req,res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
         post_url:req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id    
     })
     .then(dbPostData => {
         res.json(dbPostData);
@@ -165,7 +167,7 @@ router.put('/upvote', (req, res) => {
     // });
 
 
-router.put('/:id', (req,res)=> {
+router.put('/:id', withAuth, (req,res)=> {
     Post.update(
         {
             title: req.body.title
@@ -190,7 +192,7 @@ router.put('/:id', (req,res)=> {
     });
 });
 
-router.delete('/:id', (req, res)=> {
+router.delete('/:id',withAuth,  (req, res)=> {
     Post.destroy({
         where: {
             id:req.params.id
